f70c93e49ed479e365400c483deb84ffee1771ec
Hendra Manudinata <hendra@manoedinata.me>
2024-02-20 17:26:02 +0700

    Boyera: Mulai pengembangan tampilan UI
    Mulai pengembangan tampilan frontend. Desain menggunakan bagian template
    dari Tabler [1] untuk tampilan yang modern dan elegan.
    
    Aset (static files) masih berantakan dan belum disortir karena masih
    work-in-progress. Pengembangan akan terus dilanjutkan.
    
    [1] https://tabler.io
    
    Signed-off-by: Hendra Manudinata <hendra@manoedinata.me>

--- boyera/routes/home.py
+++ boyera/routes/home.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
+from flask import render_template
 
-routes_home = Blueprint("routes_home", __name__, url_prefix="/")
+routes_home = Blueprint("routes_home", __name__, template_folder="templates", url_prefix="/")
+
+@routes_home.get("/")
+def home():
+    return render_template("routes/base.html")
