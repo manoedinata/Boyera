85c187ed7480f859cc07186a38696ba7ede21fce
Hendra Manudinata <hendra@manoedinata.me>
2024-02-20 18:22:11 +0700

    Boyera: db: Tambah model: `Siswa`
    Model ini akan digunakan untuk data siswa.
    
    Signed-off-by: Hendra Manudinata <hendra@manoedinata.me>

--- boyera/database.py
+++ boyera/database.py
@@ -4,3 +4,25 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate(db=db)
+
+class Siswa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nis = db.Column(db.Integer, nullable=False, unique=True)
+    nama = db.Column(db.String(1000), nullable=False, unique=False)
+    jenjang = db.Column(db.Integer, nullable=False, unique=False)
+    kelas = db.Column(db.String(100), nullable=False, unique=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        """https://stackoverflow.com/a/7103486"""
+        return {
+            "id": self.id,
+            "nis": self.nis,
+            "name": self.nama,
+            "jenjang": self.jenjang,
+            "kelas": self.kelas
+        }
+
+    def __repr__(self):
+        return f"<Siswa {self.nis}>"
