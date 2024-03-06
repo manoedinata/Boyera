68606c4e17469bdee9c6bf3a5e90d0c780319f68
Hendra Manudinata <manoedinata@gmail.com>
2024-03-06 08:07:44 +0700

    Boyera: Hapus kode debug yang tertinggal
    Signed-off-by: Hendra Manudinata <manoedinata@gmail.com>

--- boyera/routes/home.py
+++ boyera/routes/home.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask import render_template
 
-from flask_login import current_user
 from flask_login import login_required
 
 routes_home = Blueprint("routes_home", __name__, template_folder="templates", url_prefix="/")
@@ -9,5 +8,4 @@ routes_home = Blueprint("routes_home", __name__, template_folder="templates", ur
 @routes_home.get("/")
 @login_required
 def home():
-    print(current_user.serialize)
     return render_template("routes/home.html")
